3269a2494063ef536eccdd6d1a62533fa5a77643
Jean Kristine Jhan Estrella <jkestrella@gbox.adnu.edu.ph>
2026-04-07 21:16:15 +0800

    feat: add FallingPlatform and LevelSceneTransition for enhanced gameplay mechanics

---
+++ Assets/Scripts/SCRIPTS/SYSTEMS SCRIPT/LevelSceneTransition.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSceneTransition : MonoBehaviour
+{
+   public string playerTag = "Player"; // Tag to identify the player
+   private bool levelComplated = false; // Flag to prevent multiple triggers
+
+   private void OnTriggerEnter(Collider levelCollision)
+   {
+      //checks if player triggered the exit and has not completed the level yet
+      if (levelCollision.CompareTag(playerTag) && !levelComplated)
+      {
+         levelComplated = true; //prevents loading the scene multiple times
+         
+         int currentScene = SceneManager.GetActiveScene().buildIndex;
+         int nextScene = currentScene + 1; //calculates the next scene index
+         
+         //checks if the next scene index is within the valid range of scenes
+         if (nextScene < SceneManager.sceneCountInBuildSettings)
+         {
+            Debug.Log("Loading next scene...");
+            SceneManager.LoadScene(nextScene);
+         }
+         else
+         {
+            SceneManager.LoadScene(0);
+         }
+      }
+   }
+}
