1994af5175d0e7c274d2c80ff0900a16e0cfaad4
Zarah Gayanilo <zgayanilo@gbox.adnu.edu.ph>
2026-05-07 11:11:38 +0800

    mmyess changes
    NAAYOS NA LEVEL 1 BUG WOOHOO

--- Assets/Scripts/SCRIPTS/PLAYER SCRIPT/PlayerJournalItemInventory.cs
+++ Assets/Scripts/SCRIPTS/PLAYER SCRIPT/PlayerJournalItemInventory.cs
@@ -3,13 +3,34 @@ using UnityEngine;
 
 public class PlayerJournalItemInventory : MonoBehaviour
 {
-   
-    public List<string> inventory =  new List<string>();
+    [Header("Inventory")]
+    public List<string> inventory = new List<string>();
+
+    public System.Action OnInventoryChanged;
 
     public void AddItem(string itemName)
     {
+        if (string.IsNullOrWhiteSpace(itemName))
+            return;
+
         inventory.Add(itemName);
-        Debug.Log("Added to inventory: " + itemName);
-        Debug.Log("Added to inventory: " + string.Join(",", inventory));
+
+        Debug.Log($"Added to inventory: {itemName}");
+        Debug.Log("Inventory: " + string.Join(", ", inventory));
+
+        OnInventoryChanged?.Invoke();
+    }
+
+    public bool HasItem(string itemName)
+    {
+        return inventory.Contains(itemName);
+    }
+
+    public void RemoveItem(string itemName)
+    {
+        if (inventory.Remove(itemName))
+        {
+            OnInventoryChanged?.Invoke();
+        }
     }
 }
